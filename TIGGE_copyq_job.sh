4e631c1d9885148ead608c7d10c284304911bdc4
Eloise Wilson-Mayne <130010856+eloisewm@users.noreply.github.com>
2024-07-29 14:44:46 +1000

    Add new files for data retrieval

---
+++ TIGGE_copyq_job.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#PBS -P xv83
+#PBS -q copyq
+#PBS -l walltime=10:00:00
+#PBS -l mem=20GB
+#PBS -l storage=gdata/ia39+gdata/hh5+gdata/xv83
+#PBS -l wd
+
+module use /g/data/hh5/public/modules
+module load conda/analysis3-unstable
+
+python3 TIGGE_copyq_job.py
